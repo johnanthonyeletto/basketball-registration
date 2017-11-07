52a68003b5462023e8ab8010acfce5803a5962bf
johnanthonyeletto <johnanthony.eletto@gmail.com>
2017-11-07 16:19:31 -0500

    Setting up post request

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,12 +7,14 @@
 <section class="container">
   <table class="table table-striped table-hover" id="gameTable">
     <thead>
+      <th class="hidden">Game ID</th>
       <th>Game</th>
       <th>Date</th>
     </thead>
     <tbody>
       @foreach($games as $game)
       <tr>
+        <td class="hidden">{{ $game->game_id }}</td>
         <td>{{ $game->game_name }} @if($game->game_required == 1) - <b style="color: red;">Required</b>@endif</td>
         <td>{{\Carbon\Carbon::parse($game->game_datetime)->format('l, M d, Y h:i A')}}</td>
       </tr>
@@ -22,11 +24,32 @@
 </section>
 
 <script>
-$(document).ready(function(){
-    $('#gameTable').DataTable({
-      paging: false
-    });
+var table = $('#gameTable').DataTable({
+  paging: false
 });
+
+$('#gameTable tbody').on( 'click', 'tr', function () {
+  if ( $(this).hasClass('selected') ) {
+    $(this).removeClass('selected');
+  }
+  else {
+    table.$('tr.selected').removeClass('selected');
+    $(this).addClass('selected');
+    
+    var selected = table.row( this ).data();
+    
+    $.ajax({
+      type: "POST",
+      url: "/api/get-game-responses",
+      data: {
+        game_id: selected[0]
+      }
+    }).done(function(response){
+      console.log(response);
+    });
+    
+  }
+} );
 </script>
 
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/get-game-responses', function(Request $request){
+  $responses = DB::table('student')->leftJoin('game_response', 'student.CWID', 'game_response.CWID')->select('student.cwid', DB::raw('CONCAT(student.first_name, " ", student.last_name) AS student_name'), 'student.instrument', 'student.scholarship', 'game_response.choice_id')->where('game_response.game_id', '=', $request['game_id'])->orWhereNull('game_response.choice_id')->get();
+  
+  return json_encode($responses);
+});
