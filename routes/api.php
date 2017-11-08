331742a3aae1c390368dc81410d72ec84c45942e
johnanthonyeletto <johnanthony.eletto@gmail.com>
2017-11-07 20:27:28 -0500

    Trying to fix api

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => ['auth:api']], function () {
 
   Route::post('/get-game-responses', function(Request $request){
     $responses = DB::table('student')->leftJoin('game_response', 'student.CWID', '=', 'game_response.CWID')->leftJoin('assignment', 'student.CWID', '=', 'assignment.CWID')->select('student.cwid', DB::raw('CONCAT(student.first_name, " ", student.last_name) AS student_name'), 'student.instrument', 'student.scholarship', 'game_response.choice_id', DB::raw("EXISTS(select * from assignment where assignment.CWID = student.CWID AND assignment.game_id = ". $request['game_id'] .") AS assigned"), DB::raw('(SELECT COUNT(assignment.CWID) from assignment JOIN game on assignment.game_id = game.game_id WHERE assignment.CWID = student.CWID AND DAYOFWEEK(game.game_datetime) IN (1,7)) AS weekend_assigned'), DB::raw('(SELECT COUNT(assignment.CWID) from assignment JOIN game on assignment.game_id = game.game_id WHERE assignment.CWID = student.CWID AND DAYOFWEEK(game.game_datetime) IN (2,3,4,5,6)) AS weekday_assigned'))->where('game_response.game_id', '=', $request['game_id'])->orWhereNull('game_response.choice_id')->groupBy('student.cwid', 'student_name', 'assigned', 'instrument', 'scholarship', 'choice_id')->orderBy('student.instrument')->get();
@@ -39,4 +38,3 @@ Route::group(['middleware' => ['auth:api']], function () {
 
     return response(200);
   });
-});
