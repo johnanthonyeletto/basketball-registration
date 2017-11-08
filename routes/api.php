2d23b0d75577f4dea9c38e6d2d9867fdceecf428
johnanthonyeletto <johnanthony.eletto@gmail.com>
2017-11-07 20:25:24 -0500

    Trying to fix api routes

--- routes/api.php
+++ routes/api.php
@@ -13,31 +13,30 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-  return $request->user();
-});
+Route::group(['middleware' => ['auth:api']], function () {
 
-Route::post('api/get-game-responses', function(Request $request){
-  $responses = DB::table('student')->leftJoin('game_response', 'student.CWID', '=', 'game_response.CWID')->leftJoin('assignment', 'student.CWID', '=', 'assignment.CWID')->select('student.cwid', DB::raw('CONCAT(student.first_name, " ", student.last_name) AS student_name'), 'student.instrument', 'student.scholarship', 'game_response.choice_id', DB::raw("EXISTS(select * from assignment where assignment.CWID = student.CWID AND assignment.game_id = ". $request['game_id'] .") AS assigned"), DB::raw('(SELECT COUNT(assignment.CWID) from assignment JOIN game on assignment.game_id = game.game_id WHERE assignment.CWID = student.CWID AND DAYOFWEEK(game.game_datetime) IN (1,7)) AS weekend_assigned'), DB::raw('(SELECT COUNT(assignment.CWID) from assignment JOIN game on assignment.game_id = game.game_id WHERE assignment.CWID = student.CWID AND DAYOFWEEK(game.game_datetime) IN (2,3,4,5,6)) AS weekday_assigned'))->where('game_response.game_id', '=', $request['game_id'])->orWhereNull('game_response.choice_id')->groupBy('student.cwid', 'student_name', 'assigned', 'instrument', 'scholarship', 'choice_id')->orderBy('student.instrument')->get();
+  Route::post('api/get-game-responses', function(Request $request){
+    $responses = DB::table('student')->leftJoin('game_response', 'student.CWID', '=', 'game_response.CWID')->leftJoin('assignment', 'student.CWID', '=', 'assignment.CWID')->select('student.cwid', DB::raw('CONCAT(student.first_name, " ", student.last_name) AS student_name'), 'student.instrument', 'student.scholarship', 'game_response.choice_id', DB::raw("EXISTS(select * from assignment where assignment.CWID = student.CWID AND assignment.game_id = ". $request['game_id'] .") AS assigned"), DB::raw('(SELECT COUNT(assignment.CWID) from assignment JOIN game on assignment.game_id = game.game_id WHERE assignment.CWID = student.CWID AND DAYOFWEEK(game.game_datetime) IN (1,7)) AS weekend_assigned'), DB::raw('(SELECT COUNT(assignment.CWID) from assignment JOIN game on assignment.game_id = game.game_id WHERE assignment.CWID = student.CWID AND DAYOFWEEK(game.game_datetime) IN (2,3,4,5,6)) AS weekday_assigned'))->where('game_response.game_id', '=', $request['game_id'])->orWhereNull('game_response.choice_id')->groupBy('student.cwid', 'student_name', 'assigned', 'instrument', 'scholarship', 'choice_id')->orderBy('student.instrument')->get();
 
-  $game_info = DB::table('game')->leftJoin('assignment', 'game.game_id', '=', 'assignment.game_id')->select(DB::raw("COUNT(assignment.game_id) AS number_assigned"), 'game.game_required')->where("game.game_id", "=", $request['game_id'])->groupBy('game.game_required')->get();
+    $game_info = DB::table('game')->leftJoin('assignment', 'game.game_id', '=', 'assignment.game_id')->select(DB::raw("COUNT(assignment.game_id) AS number_assigned"), 'game.game_required')->where("game.game_id", "=", $request['game_id'])->groupBy('game.game_required')->get();
 
-  return json_encode([
-    "game_info" => $game_info[0],
-    "responses" => $responses
-  ]);
-});
-
-Route::post('api/assign-game', function(Request $request){
-  
-  DB::table('assignment')->where('game_id', '=', $request['game_id'])->delete();
-  
-  foreach($request['student_assigned'] as $student){
-    DB::table('assignment')->insert([
-      'game_id' => $request['game_id'], 
-      'CWID' => $student
+    return json_encode([
+      "game_info" => $game_info[0],
+      "responses" => $responses
     ]);
-  }
-  
-  return response(200);
+  });
+
+  Route::post('api/assign-game', function(Request $request){
+
+    DB::table('assignment')->where('game_id', '=', $request['game_id'])->delete();
+
+    foreach($request['student_assigned'] as $student){
+      DB::table('assignment')->insert([
+        'game_id' => $request['game_id'], 
+        'CWID' => $student
+      ]);
+    }
+
+    return response(200);
+  });
 });
